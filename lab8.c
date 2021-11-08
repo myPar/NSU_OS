4e0ec51b7f5ec0cc537b1c178cc8965c509768a0
Andrew <andrebobrovs@mail.ru>
2021-11-08 20:53:42 +0700

    lab8 revisions: split main body on several functions

--- lab8.c
+++ lab8.c
@@ -75,28 +75,16 @@ int check_str_to_int_cast(char* str) {
     return (int) in_arg_value;
 }
 
-int main(int argc, char** argv) {
-    // check argc value
-    if (argc != INPUT_ARGS_COUNT + 1) {
-        // print exception message
-        printf("invalid arg count: %d, should be 1\n", argc - 1);
-        // finish the process by returning from main
-        return EXCEPTION_EXIT_CODE;
-    }
-    // count of creating threads
-    int thread_count = check_str_to_int_cast(argv[1]);
-    // count of iterations
-    int interation_count = check_str_to_int_cast(argv[2]);
-
+// check correctness of thread count value
+int check_thread_count(int thread_count, int iteration_count) {
     // not less then one thread should executing in programm
     if (thread_count < 1) {
         printf("invalid thread count: %d, should be more then 1\n", thread_count);
         return EXCEPTION_EXIT_CODE;
     }
-
     // check thread count
-    if (thread_count > interation_count) {
-        printf("invalid thread count: %d, should be less then iteration count: %d\n", thread_count, interation_count);
+    if (thread_count > iteration_count) {
+        printf("invalid thread count: %d, should be less then iteration count: %d\n", thread_count, iteration_count);
         return EXCEPTION_EXIT_CODE;
     }
     // check MAX_THREAD_COUNT excess:
@@ -104,16 +92,24 @@ int main(int argc, char** argv) {
         printf("invalid thread count: %d, should be less then MAX_THREAD_COUNT: %d\n", thread_count, MAX_THREAD_COUNT);
         return EXCEPTION_EXIT_CODE;        
     }
-    // allocate dynamic memory for array of thread args (in parent thread)
-    args *thread_args = (args*) malloc(sizeof(args)*thread_count);
-    // check malloc execution correctness
-    if (thread_args == NULL) {
-        perror("can't allocate memory for thread_args");
+    return CORRECT_EXIT_CODE;
+}
+
+// check argc value function
+int check_argc(int argc) {
+    // check argc value
+    if (argc != INPUT_ARGS_COUNT + 1) {
+        // print exception message
+        printf("invalid arg count: %d, should be 1\n", argc - 1);
+        // finish the process by returning from main
         return EXCEPTION_EXIT_CODE;
     }
+    return CORRECT_EXIT_CODE;
+}
 
-    // init args for each thread:
-    int iter_block_size = interation_count / thread_count;
+// init thread_args array: args[thread_count]
+void init_thread_args(args* thread_args, int thread_count, int iteration_count) {
+    int iter_block_size = iteration_count / thread_count;
 
     for (int i = 0; i < thread_count; i++) {
         thread_args[i].st_idx = i * iter_block_size;
@@ -121,19 +117,12 @@ int main(int argc, char** argv) {
     }
     // init end_idx for last thread (last thread is a 'main' thread)
     int last_thread_idx = thread_count - 1;
-    thread_args[last_thread_idx].end_idx = interation_count - 1;
-    
+    thread_args[last_thread_idx].end_idx = iteration_count - 1;
+}
+
+// create threads with start routine 
+int start_all_threads(pthread_t *thread_id_array, args* thread_args, int last_thread_idx) {
     // create child threads with start routin:
-    pthread_t *thread_id_array = (pthread_t*) malloc(sizeof(pthread_t) * (last_thread_idx));   // we need to remember threads indices to join() with them
-    // check malloc execution correctness
-    if (thread_id_array == NULL) {
-        perror("can't allocate memory for thread_id_array");
-        // free already allocated resources
-        free(thread_args);
-
-        return EXCEPTION_EXIT_CODE;
-    }
-
     for (int i = 0; i < last_thread_idx; i++) {
         int exc_code = pthread_create(&thread_id_array[i], NULL, calc_sum, &thread_args[i]);
         // check exception code
@@ -146,8 +135,12 @@ int main(int argc, char** argv) {
                 return EXCEPTION_EXIT_CODE;                
         }
     }
-    double pi = 0;
-    // join child threads:
+    return CORRECT_EXIT_CODE;
+}
+
+// joining all threads and collecting partial sum (including partial sum on parent thread)
+int join_threads(double *pi, pthread_t* thread_id_array, args* thread_args, int last_thread_idx) {
+    // join child threads and collect partial sum:
     for (int i = 0; i < last_thread_idx; i++) {
         int exc_code = pthread_join(thread_id_array[i], NULL);
         // check exception code
@@ -157,18 +150,68 @@ int main(int argc, char** argv) {
             return EXCEPTION_EXIT_CODE;
         }
         // increment pi on calculated partial sum
-        pi += thread_args[i].sum;
+        *pi += thread_args[i].sum;
     }
     // calc partial sum on parent thread
     calc_sum(&thread_args[last_thread_idx]);
-    pi += thread_args[last_thread_idx].sum;
-    pi *= 4;
+    *pi += thread_args[last_thread_idx].sum;
+    *pi *= 4;
+
+    return CORRECT_EXIT_CODE;
+}
+
+int main(int argc, char** argv) {
+    // check argc value:
+    if (check_argc(argc) == EXCEPTION_EXIT_CODE) {
+        return EXCEPTION_EXIT_CODE;
+    }
+    // count of creating threads
+    int thread_count = check_str_to_int_cast(argv[1]);
+    // count of iterations
+    int iteration_count = check_str_to_int_cast(argv[2]);
+
+    // check thread count:
+    if (check_thread_count(thread_count, iteration_count) == EXCEPTION_EXIT_CODE) {
+        return EXCEPTION_EXIT_CODE;
+    }
+    // idx of the last thread
+    int last_thread_idx = thread_count - 1;
+
+    // allocate dynamic memory for array of thread args (in parent thread):
+    args *thread_args = (args*) malloc(sizeof(args)*thread_count);
+    // check malloc execution correctness
+    if (thread_args == NULL) {
+        perror("can't allocate memory for thread_args");
+        return EXCEPTION_EXIT_CODE;
+    }
+    // init args for each thread:
+    init_thread_args(thread_args, thread_count, iteration_count);
+    
+    // allocate dynamic memory for array of thread id's (pthread_t array):
+    pthread_t *thread_id_array = (pthread_t*) malloc(sizeof(pthread_t) * (last_thread_idx));   // we need to remember threads indices to join() with them
+    // check malloc execution correctness
+    if (thread_id_array == NULL) {
+        perror("can't allocate memory for thread_id_array");
+        // free already allocated resources
+        free(thread_args);
+        return EXCEPTION_EXIT_CODE;
+    }
+    // create child threads with start routin:
+    if (start_all_threads(thread_id_array, thread_args, last_thread_idx) == EXCEPTION_EXIT_CODE) {
+        return EXCEPTION_EXIT_CODE;
+    }
+    double pi = 0;
+    // join child threads and collect partial sum:
+    if (join_threads(&pi, thread_id_array, thread_args, last_thread_idx) == EXCEPTION_EXIT_CODE) {
+        return EXCEPTION_EXIT_CODE;
+    }
     // print the result
-    printf("pi calc value - %.15g iteration count: %d, thread count: %d \n", pi, interation_count, thread_count);
+    printf("pi calc value - %.15g iteration count: %d, thread count: %d \n", pi, iteration_count, thread_count);
 
     // free resources (in parent thread)
     FREE_RESOURCES
-    pthread_exit(CORRECT_EXIT_CODE);
+
+    return CORRECT_EXIT_CODE;
 }
 
 #undef CORRECT_EXIT_CODE
